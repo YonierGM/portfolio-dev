7813809b9c818b6b4473bea03621a7fe32c18cc2
YonierGM <yoniermosquera55@gmail.com>
2025-09-06 21:14:42 -0500

    add widget chat

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import { ProjectDetail } from './pages/ProjectDetail'
 import { Contact } from './pages/Contact'
 import { AboutMe } from './pages/AboutMe';
 
+import ChatWidget from './components/Chat-Widget/Chat';
 function App() {
 
   return (
     <>
+    <ChatWidget />
     <section className='main grid grid-rows-[auto_1fr] min-h-screen bg-[#F0F2F5] gap-4'>
        <BrowserRouter>
         <div className="header z-50 w-full sticky top-0 max-sm:h-14 sm:h-16">
